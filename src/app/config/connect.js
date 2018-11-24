754c9408efba9e4a8e73679ed3d9dc3dd7b91cb9
swapnil2504 <swap.2504@gmail.com>
2018-11-24 18:25:26 +0530

    api backend url

---
+++ src/app/config/connect.js
@@ -0,0 +1,4 @@
+/**
+ * Diffent Connection Configs */
+
+export const BACKEND_BASE_URL = 'http://13.233.86.110:8000/';
--- src/app/constants/api.js
+++ src/app/constants/api.js
@@ -1,6 +1,9 @@
+import {BACKEND_BASE_URL} from "../config/connect";
+
 /***
  * API Connection URLs
  * */
+ export const API_URL = BACKEND_BASE_URL + 'api';
 
 export const SIGNUP_URL = '';
 export const LOGIN_URL = '';
