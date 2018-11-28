a809e87006ec185115267a73aeab6277a37ac728
swapnil2504 <swap.2504@gmail.com>
2018-11-28 17:01:02 +0530

    clinic staff data initiated

--- src/app/components/settings/options/practice-staff/PracticeStaff.js
+++ src/app/components/settings/options/practice-staff/PracticeStaff.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DynamicFieldsForm from "../../../common/DynamicFieldsForm";
-import {Button, Card, Form, Icon, Row, Table} from "antd";
+import {Button, Card, Form, Icon, Divider, Tag , Row, Table} from "antd";
 import {CHECKBOX_FIELD, INPUT_FIELD, RADIO_FIELD, SELECT_FIELD} from "../../../../constants/dataKeys";
 import {PRACTICESTAFF} from "../../../../constants/api"
 import {Link} from "react-router-dom";
 import {getAPI} from "../../../../utils/common"
+
+const { Column, ColumnGroup } = Table;
 
 class PracticeDetails extends React.Component {
     constructor(props) {
@@ -35,6 +37,42 @@ class PracticeDetails extends React.Component {
             </h2>
             <Card>
                 <Table>
+                <Column
+                  title="Name"
+                  dataIndex="name"
+                  key="name"
+                  />
+                  <Column
+                  title="Roles"
+                  dataIndex="role"
+                  key="role"
+                  render={tags => (
+                    <span>
+                      {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+                    </span>
+                  )}
+                  />
+                  <Column
+                  title="Login Status"
+                  dataIndex="loginstatus"
+                  key="loginstatus"
+                  />
+                  <Column
+                  title="Last Logged in"
+                  dataIndex="lastloggedin"
+                  key="lastloggedin"
+                  />
+                  <Column
+                  title="Action	"
+                  key="action"
+                  render={(text, record) => (
+                    <span>
+                      <a href="javascript:;">edit {record.name}</a>
+                      <Divider type="vertical" />
+                      <a href="javascript:;">Delete</a>
+                    </span>
+                  )}
+                  />
 
                 </Table>
             </Card>
