01fb1145e73c3a1dde70554e28c15e57c8a1c20a
swapnil2504 <swap.2504@gmail.com>
2018-11-30 02:25:45 +0530

    practice staff

--- src/app/components/settings/options/practice-staff/PracticeStaff.js
+++ src/app/components/settings/options/practice-staff/PracticeStaff.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DynamicFieldsForm from "../../../common/DynamicFieldsForm";
-import {Button, Card, Form, Icon, Divider, Tag , Row, Table} from "antd";
+import {Button, Card, Form, Icon,Menu, Divider, Tag , Row, Table} from "antd";
 import {CHECKBOX_FIELD, INPUT_FIELD, RADIO_FIELD, SELECT_FIELD} from "../../../../constants/dataKeys";
 import {PRACTICESTAFF} from "../../../../constants/api"
 import {Link} from "react-router-dom";
@@ -11,7 +11,9 @@ const { Column, ColumnGroup } = Table;
 class PracticeDetails extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+          current: 'staff',
+        }
     }
 
     componentDidMount() {
@@ -24,11 +26,17 @@ class PracticeDetails extends React.Component {
         getAPI(PRACTICESTAFF, successFn, errorFn);
       }
 
+    handleClick = (e) => {
+        console.log('click ', e);
+        this.setState({
+          current: e.key,
+        });
+      }
 
 
     render() {
         return <Row>
-            <h2>Title
+            <h2>Practice Staff
                 <Link to="/settings/clinics-staff/adddoctor">
                     <Button type="primary" style={{float: 'right'}}>
                         <Icon type="plus"/>&nbsp;Add
@@ -36,45 +44,116 @@ class PracticeDetails extends React.Component {
                 </Link>
             </h2>
             <Card>
+            <Menu
+               onClick={this.handleClick}
+               selectedKeys={[this.state.current]}
+               mode="horizontal"
+             >
+              <Menu.Item key="staff">
+                 <Icon type="mail" />Manage Staff
                 <Table>
-                <Column
-                  title="Name"
-                  dataIndex="name"
-                  key="name"
-                  />
                   <Column
-                  title="Roles"
-                  dataIndex="role"
-                  key="role"
-                  render={tags => (
-                    <span>
-                      {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
-                    </span>
-                  )}
-                  />
-                  <Column
-                  title="Login Status"
-                  dataIndex="loginstatus"
-                  key="loginstatus"
-                  />
-                  <Column
-                  title="Last Logged in"
-                  dataIndex="lastloggedin"
-                  key="lastloggedin"
-                  />
-                  <Column
-                  title="Action	"
-                  key="action"
-                  render={(text, record) => (
-                    <span>
-                      <a href="javascript:;">edit {record.name}</a>
-                      <Divider type="vertical" />
-                      <a href="javascript:;">Delete</a>
-                    </span>
-                  )}
-                  />
+                    title="Name"
+                    dataIndex="name"
+                    key="name"
+                    />
+                    <Column
+                    title="Roles"
+                    dataIndex="role"
+                    key="role"
+                    render={tags => (
+                      <span>
+                        {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+                      </span>
+                    )}
+                    />
+                    <Column
+                    title="Login Status"
+                    dataIndex="loginstatus"
+                    key="loginstatus"
+                    />
+                    <Column
+                    title="Last Logged in"
+                    dataIndex="lastloggedin"
+                    key="lastloggedin"
+                    />
+                    <Column
+                    title="Action	"
+                    key="action"
+                    render={(text, record) => (
+                      <span>
+                      <Link to="/settings/clinics-staff/adddoctor">
+                        <a>edit {record.name}</a></Link>
+                        <Divider type="vertical" />
+                        <a href="javascript:;">Delete</a>
+                      </span>
+                    )}
+                    />
 
                 </Table>
+              </Menu.Item>
+              <Menu.Item key="notification">
+                   <Icon type="mail" />Staff Notification
+                  <Table>
+                    <Column
+                      title="Name"
+                      dataIndex="name"
+                      key="name"
+                      />
+                      <Column
+                      title="Confirmation SMS"
+                      dataIndex="loginstatus"
+                      key="confirmationSms"
+                      />
+                      <Column
+                      title="Schedule SMS"
+                      dataIndex="lastloggedin"
+                      key="ScheduleSMS"
+                      />
+                      />
+                      <Column
+                      title="Confirmation EMAIL"
+                      dataIndex="loginstatus"
+                      key="ConfirmationEMAIL"
+                      />
+                      />
+                      <Column
+                      title="Schedule EMAIL"
+                      dataIndex="loginstatus"
+                      key="ScheduleEMAIL"
+                      />
+
+                  </Table>
+                </Menu.Item>
+                <Menu.Item key="timing">
+                     <Icon type="mail" />Doctors visit Timing
+                    <Table>
+                      <Column
+                        title="Name"
+                        dataIndex="name"
+                        key="name"
+                        />
+                        <Column
+                        title="Visit Timing"
+                        dataIndex="loginstatus"
+                        key="VisitTiming"
+                        />
+
+                        <Column
+                        title="Action	"
+                        key="action"
+                        render={(text, record) => (
+                          <span>
+                          <Link to="/settings/clinics-staff/adddoctor">
+                            <a>edit {record.name}</a></Link>
+                          </span>
+                        )}
+                        />
+
+                    </Table>
+                  </Menu.Item>
+                </Menu>
+
             </Card>
         </Row>
     }
