767b5782441329c86d2f8dbeddee8e79a51d9a82
Mohit Gupta <itsmohitt@gmail.com>
2018-12-09 12:51:32 +0530

    Added App Sider Changes

--- src/app/components/core/AppSider.js
+++ src/app/components/core/AppSider.js
@@ -24,16 +24,39 @@ class AppSider extends React.Component {
             onCollapse={(collapsed, type) => {
                 console.log(collapsed, type);
             }}>
-            <img src={Applogo} alt="" style={{width:'100%'}}/>
+            <img src={Applogo} alt="" style={{width: '100%'}}/>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
                 <Menu.Item key="1">
-                    <Icon type="user"/>
-                    <span className="nav-text">nav 1</span>
+                    <Link to="/calender">
+                        <Icon type="schedule"/>
+                        <span className="nav-text">Calender</span>
+                    </Link>
                 </Menu.Item>
+                <SubMenu key="sub2" title={<span><Icon type="user"/><span>Paitients</span></span>}>
+                    <Menu.ItemGroup key="g1" title="Patient">
+                        <Menu.Item key="17"><Link to="/patients/profile">Profile</Link></Menu.Item>
+                        <Menu.Item key="18"><Link to="/patients/appointments">Appointments</Link></Menu.Item>
+                        <Menu.Item key="19"><Link to="/patients/communications">Communications</Link></Menu.Item>
+                    </Menu.ItemGroup>
+                    <Menu.ItemGroup key="g2" title="EMR">
+                        {/*<SubMenu key="nestedsub1" title={<span>EMR</span>}>*/}
+                        <Menu.Item key="20"><Link to="/emr/patients/vitalsigns">Vitalsings</Link></Menu.Item>
+                        <Menu.Item key="21"><Link to="/emr/patients/clinicnotes">Profile</Link></Menu.Item>
+                        <Menu.Item key="22"><Link to="/emr/patients/workdone">Profile</Link></Menu.Item>
+                        <Menu.Item key="23"><Link to="/emr/patients/files">Profile</Link></Menu.Item>
+                        <Menu.Item key="24"><Link to="/emr/patients/prescriptions">Profile</Link></Menu.Item>
+                    </Menu.ItemGroup>
+                    {/*</SubMenu>*/}
+                    <Menu.ItemGroup key="g3" title="Billing">
+                        <Menu.Item key="25"><Link to="/billing/patients/invoices">Invoices</Link></Menu.Item>
+                        <Menu.Item key="26"><Link to="/billing/patients/payments">Payments</Link></Menu.Item>
+
+                    </Menu.ItemGroup>
+                </SubMenu>
                 <SubMenu key="sub1" title={<span><Icon type="setting"/><span>Settings</span></span>}>
                     <Menu.Item key="5"><Link to="/settings/clinics">Practice Details</Link></Menu.Item>
                     <Menu.Item key="6"><Link to="/settings/clinics-staff">Practice Staff</Link></Menu.Item>
-{/*                    <Menu.Item key="7"><Link to="/settings/communication-settings">Communication Settings</Link></Menu.Item>*/}
+                    {/*                    <Menu.Item key="7"><Link to="/settings/communication-settings">Communication Settings</Link></Menu.Item>*/}
                     <Menu.Item key="8"><Link to="/settings/procedures">Procedure Catalog</Link></Menu.Item>
                     <Menu.Item key="9"><Link to="/settings/billing">Billing</Link></Menu.Item>
                     <Menu.Item key="10"><Link to="/settings/loyalty">Loyalty</Link></Menu.Item>
@@ -43,24 +66,6 @@ class AppSider extends React.Component {
                     <Menu.Item key="14"><Link to="/settings/labs">Labs</Link> </Menu.Item>
                     <Menu.Item key="15"><Link to="/settings/medical-history"> Medical History</Link></Menu.Item>
                     <Menu.Item key="16"><Link to="/settings/expense-types"> Expense Types</Link></Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="user"/><span>Paitients</span></span>}>
-                  <Menu.Item key="17"><Link to="/patients/profile">Profile</Link></Menu.Item>
-                  <Menu.Item key="18"><Link to="/patients/appointments">Appointments</Link></Menu.Item>
-                  <Menu.Item key="19"><Link to="/patients/communications">Communications</Link></Menu.Item>
-                  <SubMenu key="nestedsub1" title={<span>EMR</span>}>
-                    <Menu.Item key="20"><Link to="emr/patients/vitalsigns">Vitalsings</Link></Menu.Item>
-                    <Menu.Item key="21"><Link to="emr/patients/clinicnotes">Profile</Link></Menu.Item>
-                    <Menu.Item key="22"><Link to="emr/patients/workdone">Profile</Link></Menu.Item>
-                    <Menu.Item key="23"><Link to="emr/patients/files">Profile</Link></Menu.Item>
-                    <Menu.Item key="24"><Link to="emr/patients/prescriptions">Profile</Link></Menu.Item>
-                  </SubMenu>
-                  <SubMenu key="nestedsub2" title={<span>Billing</span>}>
-                  <Menu.Item key="25"><Link to="billing/patients/invoices">Invoices</Link></Menu.Item>
-                  <Menu.Item key="26"><Link to="billing/patients/payments">Payments</Link></Menu.Item>
-
-                  </SubMenu>
-
                 </SubMenu>
 
 
