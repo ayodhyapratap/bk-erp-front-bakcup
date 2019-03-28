f17dd58942b865d0d8d2d831caad78fa24f44ab7
NITISHSHARMA12 <nsniteshsharma47@gmail.com>
2019-03-28 18:37:16 +0530

    delete and update manufactures

--- src/app/components/inventory/manufacture/AddManufacture.js
+++ src/app/components/inventory/manufacture/AddManufacture.js
@@ -3,6 +3,7 @@ import React from "react";
 import {
     INPUT_FIELD,
     SUCCESS_MSG_TYPE,
+    TEXT_FIELD,
 } from "../../../constants/dataKeys";
 import DynamicFieldsForm from "../../common/DynamicFieldsForm";
 import {displayMessage, getAPI, interpolate} from "../../../utils/common";
@@ -59,7 +60,12 @@ export default class AddManufacture extends React.Component {
             key: "name",
             initialValue: this.state.editData ? this.state.editData.name : null,
             type: INPUT_FIELD
-        },];
+        },{
+            label: 'Details',
+            key: 'description',
+            type:TEXT_FIELD
+        }, 
+        ];
 
 
         let editformProp;
