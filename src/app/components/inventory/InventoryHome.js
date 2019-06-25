c1231520604706fba39b81c7c390a07bec0376ff
nitishsharma12 <nsniteshsharma47@gmail.com>
2019-06-25 18:53:21 +0530

    back office

--- src/app/components/inventory/InventoryHome.js
+++ src/app/components/inventory/InventoryHome.js
@@ -7,6 +7,7 @@ import ManufactureList from "./manufacture/ManufactureList";
 import LabList from "./labs/LabList";
 import ActivityList from "./activities/ActivityList";
 import InventoryItemList from "./inventoryItem/InventoryItemList";
+import PermissionDenied from "../common/errors/PermissionDenied";
 
 const {Content} = Layout;
 export default class InventoryHome extends React.Component {
@@ -15,6 +16,8 @@ export default class InventoryHome extends React.Component {
     }
 
     render() {
+        let that =this;
+        console.log("props hai kya")
         return <Content className="main-container" style={{
             margin: '24px 16px',
             // padding: 24,
@@ -23,12 +26,19 @@ export default class InventoryHome extends React.Component {
         }}>
 
             <Switch>
-                <Route path="/inventory/vendor" render={(route) => <VendorList {...this.props}/>}/>
-                <Route path="/inventory/expenses" render={(route) => <ExpensesList {...this.props}/>}/>
-                <Route path="/inventory/manufacture" render={(route) => <ManufactureList {...this.props}/>}/>
-                <Route path="/inventory/lab" render={(route) => <LabList {...this.props}/>}/>
-                <Route path="/inventory/activity" render={(route) => <ActivityList {...this.props}/>}/>
-                <Route path="/inventory" render={(route)=> <InventoryItemList {...route} {...this.props}/>}/>
+                <Route path="/inventory/vendor" render={(route) => 
+                    <VendorList {...this.props} {...route}/>}/>
+                <Route path="/inventory/expenses" render={(route) =>(that.props.activePracticePermissions.Expenses || that.props.allowAllPermissions ?
+                    <ExpensesList {...this.props} {...route}/>:<PermissionDenied/>)}/>
+
+                <Route path="/inventory/manufacture" render={(route) => 
+                    <ManufactureList {...this.props} {...route}/>}/>
+                <Route path="/inventory/lab" render={(route) => (that.props.activePracticePermissions.Labs || that.props.allowAllPermissions ?
+                    <LabList {...this.props}/>:<PermissionDenied/>)}/>
+                <Route path="/inventory/activity" render={(route) =>(that.props.activePracticePermissions.Activities || that.props.allowAllPermissions ?
+                    <ActivityList {...this.props} {...route}/>:<PermissionDenied/>)}/>
+                <Route path="/inventory" render={(route)=> (that.props.activePracticePermissions.Inventory || that.props.allowAllPermissions ?
+                    <InventoryItemList {...route} {...this.props}/>:<PermissionDenied/>)}/>
             </Switch>
         </Content>
     }
