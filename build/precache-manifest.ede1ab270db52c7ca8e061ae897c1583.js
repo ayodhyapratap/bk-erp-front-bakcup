51d40fb924d62c39e3a60eff8263263e4ef2638c
Mohit Gupta <itsmohitt@gmail.com>
2019-08-01 13:33:17 +0530

    Adding BULD

---
+++ build/precache-manifest.ede1ab270db52c7ca8e061ae897c1583.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3378a129f4cbafecaf7e1cdc20d90edc",
+    "url": "/static/media/kidneycarelogo.3378a129.png"
+  },
+  {
+    "revision": "9ed13bbe02debb8e23db19a4e6603c2e",
+    "url": "/static/media/404.9ed13bbe.svg"
+  },
+  {
+    "revision": "271cd790a551ce054e4eb56168db3cee",
+    "url": "/static/media/403.271cd790.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d15262be0126576b1af",
+    "url": "/static/js/main.3d15262b.chunk.js"
+  },
+  {
+    "revision": "a961880d91f863215653",
+    "url": "/static/js/1.a961880d.chunk.js"
+  },
+  {
+    "revision": "3d15262be0126576b1af",
+    "url": "/static/css/main.25aac9d7.chunk.css"
+  },
+  {
+    "revision": "a961880d91f863215653",
+    "url": "/static/css/1.30eb047b.chunk.css"
+  },
+  {
+    "revision": "d06b58c0ccb323bd4c40a01181310d90",
+    "url": "/index.html"
+  }
+];
