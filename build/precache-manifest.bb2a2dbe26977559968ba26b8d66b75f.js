56eafb2f0155703349552f80c371ff69ccc3bb46
shivam chaurasia <sc6500@gmail.com>
2019-08-19 14:49:07 +0530

    ADDING BUILD

---
+++ build/precache-manifest.bb2a2dbe26977559968ba26b8d66b75f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e573eac97e8b1d7a75726ee6160346a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31362dba5cf16ab1e9fa",
+    "url": "/static/css/2.5dbafdeb.chunk.css"
+  },
+  {
+    "revision": "6eab2f859d1630a301ed",
+    "url": "/static/css/main.257f8a86.chunk.css"
+  },
+  {
+    "revision": "31362dba5cf16ab1e9fa",
+    "url": "/static/js/2.40b5643a.chunk.js"
+  },
+  {
+    "revision": "6eab2f859d1630a301ed",
+    "url": "/static/js/main.2000e018.chunk.js"
+  },
+  {
+    "revision": "e2d4d38751c16d78dfc3",
+    "url": "/static/js/runtime~main.ab6fb9cb.js"
+  },
+  {
+    "revision": "3378a129f4cbafecaf7e1cdc20d90edc",
+    "url": "/static/media/kidneycarelogo.3378a129.png"
+  }
+]);
