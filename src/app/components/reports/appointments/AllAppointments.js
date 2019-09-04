c75facce48e435ae101e14f012b86367b6e49554
nitishsharma12 <nsniteshsharma47@gmail.com>
2019-09-04 19:06:43 +0530

    AppointmentReports

--- src/app/components/reports/appointments/AllAppointments.js
+++ src/app/components/reports/appointments/AllAppointments.js
@@ -1,23 +1,13 @@
 import React from "react";
 import {Button, Card, Col, Icon, Radio, Row, Table} from "antd";
 import {APPOINTMENT_REPORTS} from "../../../constants/api";
-import {ALL_APPOINTMENT,APPOINTMENT_FOR_EACH_CATEGORY ,CANCELLATION_NUMBERS,AVERAGE_WAITING_ENGAGED_TIME_DAY_WISE,AVERAGE_WAITING_ENGAGED_TIME_MONTH_WISE,
+import {APPOINTMENT_FOR_EACH_CATEGORY ,CANCELLATION_NUMBERS,AVERAGE_WAITING_ENGAGED_TIME_DAY_WISE,AVERAGE_WAITING_ENGAGED_TIME_MONTH_WISE,
     REASONS_FOR_CANCELLATIONS,DAILY_APPOINTMENT_COUNT,APPOINTMENT_FOR_EACH_DOCTOR, MONTHLY_APPOINTMENT_COUNT,APPOINTMENT_FOR_EACH_PATIENT_GROUP}
-from "../../../constants/dataKeys";
+    from "../../../constants/dataKeys";
 import {getAPI, displayMessage, interpolate} from "../../../utils/common";
 import {APPOINTMENT_RELATED_REPORT} from "../../../constants/hardData";
 import moment from "moment"
-import AllAppointments from "./AllAppointments";
-import AppointmentByCategory from "./AppointmentByCategory";
-import AppointmentForEachDoctor from "./AppointmentForEachDoctor";
-import AppointmentForEachPatientGroup from "./AppointmentForEachPatientGroup";
-import AverageWaitingOrEngagedTimeDayWise from "./AverageWaitingOrEngagedTimeDayWise";
-import AverageWaitingOrEngagedTimeMonthWise from "./AverageWaitingOrEngagedTimeMonthWise";
-import CancellationsNumbers from "./CancellationsNumbers";
-import DailyAppointmentCount from "./DailyAppointmentCount";
-import MonthlyAppointmentCount from './MonthlyAppointmentCount'
-import ReasonsForCancellations from "./ReasonsForCancellations";
-
+import CustomizedTable from "../../common/CustomizedTable";
 
 export default class AppointmentsReportHome extends React.Component {
     constructor(props) {
@@ -26,8 +16,7 @@ export default class AppointmentsReportHome extends React.Component {
             appointmentReports: [],
             startDate: this.props.startDate,
             endDate: this.props.endDate,
-            loading: true,
-            filterReport:'all',
+            loading: true
         }
         this.loadAppointmentReport = this.loadAppointmentReport.bind(this);
         this.loadAppointmentReport();
@@ -69,17 +58,8 @@ export default class AppointmentsReportHome extends React.Component {
         });
     }
 
-    onChangeHandle =(type,value)=>{
-        let that=this;
-        this.setState({
-            [type]:value.target.value,
-        },function () {
-            that.loadAppointmentReport();
-        })
-    }
-
     render() {
-        let that=this;
+        console.log("startApp",this.props.startDate)
         const columns = [{
             title: 'Date',
             key: 'date',
@@ -154,45 +134,15 @@ export default class AppointmentsReportHome extends React.Component {
 
 
         return <div>
-            <h2>Appointments Report
+            <h2>All Appointments Report
                 {/*<Button.Group style={{float: 'right'}}>*/}
                 {/*<Button><Icon type="mail"/> Mail</Button>*/}
                 {/*<Button><Icon type="printer"/> Print</Button>*/}
                 {/*</Button.Group>*/}
             </h2>
-            <Card>
-                <Row gutter={16}>
-                    <Col span={18}>
-                       {this.state.filterReport == ALL_APPOINTMENT ?< AllAppointments {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == APPOINTMENT_FOR_EACH_CATEGORY?<AppointmentByCategory {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == APPOINTMENT_FOR_EACH_DOCTOR?<AppointmentForEachDoctor {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == APPOINTMENT_FOR_EACH_PATIENT_GROUP?<AppointmentForEachPatientGroup {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == AVERAGE_WAITING_ENGAGED_TIME_DAY_WISE?<AverageWaitingOrEngagedTimeDayWise {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == AVERAGE_WAITING_ENGAGED_TIME_MONTH_WISE?<AverageWaitingOrEngagedTimeMonthWise {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == CANCELLATION_NUMBERS?<CancellationsNumbers {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == DAILY_APPOINTMENT_COUNT?<DailyAppointmentCount {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport == MONTHLY_APPOINTMENT_COUNT?<MonthlyAppointmentCount {...this.props} filterReport={this.state.filterReport}/>:null}
-                       {this.state.filterReport==REASONS_FOR_CANCELLATIONS?<ReasonsForCancellations {...this.props} filterReport={this.state.filterReport}/>:null}
+            <CustomizedTable loading={this.state.loading} columns={columns} size={'small'}
+                             dataSource={this.state.appointmentReports}/>
 
-                    </Col>
-                    <Col span={6}>
-                        <Radio.Group buttonStyle="solid" defaultValue="all"  onChange={(value)=>this.onChangeHandle('filterReport',value)}>
-                            <h2>Appointments</h2>
-                            <Radio.Button style={{width: '100%', backgroundColor: 'transparent', border: '0px'}}
-                                          value="all">
-                                All Appointemnts
-                            </Radio.Button>
-                            <p><br/></p>
-                            <h2>Related Reports</h2>
-                            {APPOINTMENT_RELATED_REPORT.map((item) => <Radio.Button
-                                style={{width: '100%', backgroundColor: 'transparent', border: '0px'}}
-                                value={item.value}>
-                                {item.name}
-                            </Radio.Button>)}
-                        </Radio.Group>
-                    </Col>
-                </Row>
-            </Card>
         </div>
     }
 }
---
+++ src/app/components/reports/appointments/AverageWaitingOrEngagedTimeMonthWise.js
@@ -0,0 +1,143 @@
+import React from "react";
+import {APPOINTMENT_REPORTS} from "../../../constants/api";
+import {getAPI, displayMessage, interpolate} from "../../../utils/common";
+import moment from "moment"
+import CustomizedTable from "../../common/CustomizedTable";
+
+export default class AverageWaitingOrEngagedTimeMonthWise extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            appointmentReports: [],
+            startDate: this.props.startDate,
+            endDate: this.props.endDate,
+            loading: true
+        }
+        this.loadAppointmentReport = this.loadAppointmentReport.bind(this);
+        this.loadAppointmentReport();
+    }
+
+    componentWillReceiveProps(newProps) {
+        let that = this;
+        if (this.props.startDate != newProps.startDate || this.props.endDate != newProps.endDate)
+            this.setState({
+                startDate: newProps.startDate,
+                endDate: newProps.endDate
+            },function(){
+                that.loadAppointmentReport();
+            })
+
+    }
+
+    loadAppointmentReport = () => {
+        let that = this;
+        this.setState({
+            loading:true
+        })
+        let successFn = function (data) {
+            console.log(data);
+            that.setState({
+                appointmentReports: data.data,
+                loading: false
+            });
+            console.log(that.state.appointmentReports);
+        };
+        let errorFn = function () {
+            that.setState({
+                loading: false
+            })
+        };
+        getAPI(interpolate(APPOINTMENT_REPORTS, [this.props.active_practiceId]), successFn, errorFn, {
+            start: this.state.startDate.format('YYYY-MM-DD'),
+            end: this.state.endDate.format('YYYY-MM-DD')
+        });
+    }
+
+    render() {
+        console.log("startApp",this.props.startDate)
+        const columns = [{
+            title: 'Date',
+            key: 'date',
+            render: (text, record) => (
+                <span>
+                {moment(record.schedule_at).format('LL')}
+                  </span>
+            ),
+        }, {
+            title: 'Scheduled At	',
+            key: 'time',
+            render: (text, record) => (
+                <span>
+                  {moment(record.schedule_at).format('HH:mm')}
+
+                  </span>
+            ),
+        }, {
+            title: 'Check-in At',
+            dataIndex: 'waiting',
+            key: 'waiting',
+            render: (text, record) => (
+                <span>
+                {record.waiting ? moment(record.waiting).format('lll') : ''}
+                  </span>
+            ),
+        }, {
+            title: 'Waited For (hh:mm:ss)',
+            dataIndex: 'age',
+            key: 'age',
+            render: (age, record) => (<span>
+                {record.engaged ? moment(record.engaged).from(moment(record.waiting))
+                    : ''}
+            </span>)
+        }, {
+            title: 'Engaged At',
+            dataIndex: 'engaged',
+            key: 'engaged',
+            render: (text, record) => (
+                <span>
+                {record.engaged ? moment(record.engaged).format('lll') : ''}
+                  </span>
+            ),
+        }, {
+            title: 'Checkout At',
+            dataIndex: 'checkout',
+            key: 'checkout',
+            render: (text, record) => (
+                <span>
+                {record.checkout ? moment(record.checkout).format('lll') : ''}
+                  </span>
+            ),
+        }, {
+            title: 'Patient',
+            dataIndex: 'patient',
+            key: 'patient_name',
+            render: (item, record) => <span>{item.user.first_name}</span>
+        }, {
+            title: 'Doctor',
+            dataIndex: 'doctor',
+            key: 'address',
+            render: (text, record) => <span>{record.doctor_data ? record.doctor_data.user.first_name : null}</span>
+        }, {
+            title: 'Category',
+            dataIndex: 'category',
+            key: 'address',
+            render: (text, record) => <span>{record.category_data ? record.category_data.name : null}</span>
+        }];
+
+
+
+
+
+        return <div>
+            <h2>Average Waiting/engaged Time Month Wise
+                {/*<Button.Group style={{float: 'right'}}>*/}
+                {/*<Button><Icon type="mail"/> Mail</Button>*/}
+                {/*<Button><Icon type="printer"/> Print</Button>*/}
+                {/*</Button.Group>*/}
+            </h2>
+            <CustomizedTable loading={this.state.loading} columns={columns} size={'small'}
+                             dataSource={this.state.appointmentReports}/>
+
+        </div>
+    }
+}
