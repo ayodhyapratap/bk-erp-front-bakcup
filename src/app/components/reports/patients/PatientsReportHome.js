1055f893000d469db40462a4697acacab9bb7f36
Mohit Gupta <itsmohitt@gmail.com>
2019-09-06 14:37:39 +0530

    Adding Panel Transition

--- src/app/components/reports/patients/PatientsReportHome.js
+++ src/app/components/reports/patients/PatientsReportHome.js
@@ -1,7 +1,14 @@
 import React from "react";
-import {Button, Card, Col, Icon, Radio, Row, Table,Divider} from "antd";
+import {Button, Card, Col, Icon, Radio, Row, Table, Divider} from "antd";
 import {PATIENTS_REPORTS} from "../../../constants/api";
-import {NEW_PATIENTS,DAILY_NEW_PATIENTS,PATIENTS_FIRST_APPOINTMENT,MONTHLY_NEW_PATIENTS,NEW_MEMBERSHIP,EXPIRING_MEMBERSHIP} from "../../../constants/dataKeys";
+import {
+    NEW_PATIENTS,
+    DAILY_NEW_PATIENTS,
+    PATIENTS_FIRST_APPOINTMENT,
+    MONTHLY_NEW_PATIENTS,
+    NEW_MEMBERSHIP,
+    EXPIRING_MEMBERSHIP
+} from "../../../constants/dataKeys";
 import {PATIENTS_RELATED_REPORT} from "../../../constants/hardData";
 import {getAPI, displayMessage, interpolate} from "../../../utils/common";
 import moment from "moment"
@@ -20,14 +27,16 @@ export default class PatientsReportHome extends React.Component {
         this.state = {
             startDate: this.props.startDate,
             endDate: this.props.endDate,
-            type:'DETAILED',
-            loading:true,
-            advancedOptionShow:true,
+            type: 'DETAILED',
+            loading: true,
+            advancedOptionShow: true,
+            sidePanelColSpan: 4
         }
         this.loadNewPatient = this.loadNewPatient.bind(this);
     }
+
     componentDidMount() {
-        if (this.state.type==NEW_PATIENTS){
+        if (this.state.type == NEW_PATIENTS) {
             this.loadNewPatient();
         }
 
@@ -41,18 +50,19 @@ export default class PatientsReportHome extends React.Component {
                 startDate: newProps.startDate,
                 endDate: newProps.endDate
             }, function () {
-                if (this.state.type==NEW_PATIENTS){
+                if (this.state.type == NEW_PATIENTS) {
                     this.loadNewPatient();
                 }
             })
     }
+
     loadNewPatient() {
         let that = this;
 
         let successFn = function (data) {
             that.setState({
                 report: data.results,
-                total:data.count,
+                total: data.count,
                 loading: false
             });
         };
@@ -61,21 +71,21 @@ export default class PatientsReportHome extends React.Component {
                 loading: false
             })
         };
-        let apiParams={
-            type:this.state.type,
+        let apiParams = {
+            type: this.state.type,
         }
-        if(this.state.startDate){
-            apiParams.from_date=this.state.startDate.format('YYYY-MM-DD');
-            apiParams.to_date= this.state.endDate.format('YYYY-MM-DD');
+        if (this.state.startDate) {
+            apiParams.from_date = this.state.startDate.format('YYYY-MM-DD');
+            apiParams.to_date = this.state.endDate.format('YYYY-MM-DD');
         }
 
-        getAPI(PATIENTS_REPORTS, successFn, errorFn,apiParams);
+        getAPI(PATIENTS_REPORTS, successFn, errorFn, apiParams);
     }
 
-    onChangeHandle =(type,value)=>{
-        let that=this;
+    onChangeHandle = (type, value) => {
+        let that = this;
         this.setState({
-            [type]:value.target.value,
+            [type]: value.target.value,
         })
     }
     // advancedOption(value){
@@ -84,26 +94,43 @@ export default class PatientsReportHome extends React.Component {
     //         advancedOptionShow:value,
     //     })
     // }
+    changeSidePanelSize = (sidePanel) => {
+        this.setState({
+            sidePanelColSpan: sidePanel ? 0 : 4
+        })
+    }
 
     render() {
         return <div>
-            <h2>Patients Report</h2>
+            <h2>Patients Report <Button type="primary" shape="round"
+                                        icon={this.state.sidePanelColSpan ? "double-right" : "double-left"}
+                                        style={{float: "right"}}
+                                        onClick={() => this.changeSidePanelSize(this.state.sidePanelColSpan)}>Panel</Button>
+            </h2>
             <Card>
                 <Row gutter={16}>
-                    <Col span={16}>
-                        {this.state.type==NEW_PATIENTS?
-                            <NewPatientReports {...this.props} type={NEW_PATIENTS} {...this.state}/>:null}
+                    <Col span={(24 - this.state.sidePanelColSpan)}>
 
-                        {this.state.type== DAILY_NEW_PATIENTS?<DailyNewPatientReports {...this.state}/>:null}
+                        {this.state.type == NEW_PATIENTS ?
+                            <NewPatientReports {...this.props} type={NEW_PATIENTS} {...this.state}/> : null}
 
-                        {this.state.type == EXPIRING_MEMBERSHIP?<ExpiringMembership {...this.props} type={this.state.type}/>:null}
-                        {this.state.type == PATIENTS_FIRST_APPOINTMENT?<PatientsFirstAppointment {...this.props} type={this.state.type}/>:null}
-                        {this.state.type == MONTHLY_NEW_PATIENTS?<MonthlyNewPatients  {...this.props} type={this.state.type}/>:null}
-                        {this.state.type == NEW_MEMBERSHIP ?<NewMembership  {...this.props} type={this.state.type}/>:null}
+                        {this.state.type == DAILY_NEW_PATIENTS ? <DailyNewPatientReports {...this.state}/> : null}
+
+                        {this.state.type == EXPIRING_MEMBERSHIP ?
+                            <ExpiringMembership {...this.props} type={this.state.type}/> : null}
+                        {this.state.type == PATIENTS_FIRST_APPOINTMENT ?
+                            <PatientsFirstAppointment {...this.props} type={this.state.type}/> : null}
+                        {this.state.type == MONTHLY_NEW_PATIENTS ?
+                            <MonthlyNewPatients  {...this.props} type={this.state.type}/> : null}
+                        {this.state.type == NEW_MEMBERSHIP ?
+                            <NewMembership  {...this.props} type={this.state.type}/> : null}
 
                     </Col>
-                    <Col span={8}>
-                        <Radio.Group buttonStyle="solid" defaultValue={NEW_PATIENTS} onChange={(value)=>this.onChangeHandle('type',value)}>
+
+
+                    <Col span={this.state.sidePanelColSpan} >
+                        <Radio.Group buttonStyle="solid" defaultValue={NEW_PATIENTS}
+                                     onChange={(value) => this.onChangeHandle('type', value)}>
                             <h2>Patients</h2>
                             <Radio.Button style={{width: '100%', backgroundColor: 'transparent', border: '0px'}}
                                           value={NEW_PATIENTS}>
@@ -135,6 +162,7 @@ export default class PatientsReportHome extends React.Component {
                         {/*    </Col>*/}
                         {/*</>:<a href={'#'} onClick={(value)=>this.advancedOption(true)}>Show Advanced Options</a>}*/}
                     </Col>
+
                 </Row>
             </Card>
         </div>
