8ef17d22bb5af0193e7728fde82696f142f85df2
nitishsharma12 <nsniteshsharma47@gmail.com>
2019-09-07 19:28:40 +0530

    added blood groups

--- src/app/constants/hardData.js
+++ src/app/constants/hardData.js
@@ -269,12 +269,12 @@ export const  AMOUNT_DUE_RELATED_REPORT=[
 ];
 
 export const BLOOD_GROUPS=[
-    {name:"all" ,value:'all'},
     {name:"A+", value:'A+'},
     {name:"A-", value:'A-'},
     {name: "B+", value:'B+'},
     {name:"B-", value:'B-'},
     {name:"AB+", value:'AB+'},
+    {name:"AB-", value:'AB-'},
     {name:"O+" ,value:'O+'},
     {name:"O-" ,value:'O-'},
 ];
