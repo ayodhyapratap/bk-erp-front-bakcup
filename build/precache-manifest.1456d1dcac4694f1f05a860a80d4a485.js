06e57e2bb7a17d01c894626a848725228035f5d5
Mohit Gupta <itsmohitt@gmail.com>
2019-09-16 02:26:48 +0530

    Adding Build

---
+++ build/precache-manifest.1456d1dcac4694f1f05a860a80d4a485.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c0dc0c91828260366a08adc628a2b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a8ce61df9bf248955a",
+    "url": "/static/css/2.a93e9366.chunk.css"
+  },
+  {
+    "revision": "8f84c107174ac392ca99",
+    "url": "/static/css/main.47950416.chunk.css"
+  },
+  {
+    "revision": "46a8ce61df9bf248955a",
+    "url": "/static/js/2.f531f926.chunk.js"
+  },
+  {
+    "revision": "8f84c107174ac392ca99",
+    "url": "/static/js/main.d3fe0515.chunk.js"
+  },
+  {
+    "revision": "e2d4d38751c16d78dfc3",
+    "url": "/static/js/runtime~main.ab6fb9cb.js"
+  },
+  {
+    "revision": "3378a129f4cbafecaf7e1cdc20d90edc",
+    "url": "/static/media/kidneycarelogo.3378a129.png"
+  }
+]);
