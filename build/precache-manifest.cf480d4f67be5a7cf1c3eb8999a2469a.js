26406b77601f3ae840af78c62e5a3f886c27680e
MOHIT GUPTA <mohit@MOHITs-MacBook-Air.local>
2019-10-24 12:52:48 +0530

    ADDING BUILD

---
+++ build/precache-manifest.cf480d4f67be5a7cf1c3eb8999a2469a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d0bed99a4c5576df68069743660773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25e606b140597db63297",
+    "url": "/static/css/2.91a77fe2.chunk.css"
+  },
+  {
+    "revision": "7533a8157f098ec3846a",
+    "url": "/static/css/main.385558e1.chunk.css"
+  },
+  {
+    "revision": "25e606b140597db63297",
+    "url": "/static/js/2.80a99343.chunk.js"
+  },
+  {
+    "revision": "7533a8157f098ec3846a",
+    "url": "/static/js/main.bf6d9909.chunk.js"
+  },
+  {
+    "revision": "e175b91706679c21cb7d",
+    "url": "/static/js/runtime~main.f0113334.js"
+  },
+  {
+    "revision": "3378a129f4cbafecaf7e1cdc20d90edc",
+    "url": "/static/media/kidneycarelogo.3378a129.png"
+  }
+]);
